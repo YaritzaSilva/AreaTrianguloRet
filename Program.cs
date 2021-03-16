62d67c6409075aad22527d30a868511d4277ee96
Yaritza <yaritzamicaelleb@gmail.com>
2021-03-16 19:29:35 -0300

    Tarefa AreaTrinaguloRet

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AreaTrianguloRet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string entrada1;
+            string entrada2;
+            double Area,Base, Altura;
+
+            Console.Beep();
+            Console.WriteLine("---------");
+            Console.WriteLine("Digite abaixo as medidas do triangulo, que serão calculadas");
+            Console.Write("Base: ");
+            entrada1 = Console.ReadLine();
+            Base= Convert.ToDouble(entrada1);
+            Console.Write("Altura: ");
+            entrada2 = Console.ReadLine();
+            Altura= Convert.ToDouble(entrada2);
+            
+            Area = Base*Altura/2;
+
+            Console.Beep();
+            Console.WriteLine("----------");
+            Console.ForegroundColor=ConsoleColor.DarkRed;
+            Console.WriteLine("\n---Area---");
+            Console .ResetColor ();
+            Console.WriteLine($"A área é: {Area}");
+            Console.WriteLine("----------");
+        }
+    }
+}
